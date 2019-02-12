fa59919ab4ad6276fd1f762a58caefd43bda9743
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-12 11:32:29 +0100

    fibonacci_start

---
+++ arrays/Fibonacci/suite.java
@@ -0,0 +1,8 @@
+
+
+public class suite{
+  public static void main(String[] args) {
+    int suite[]=new int[25];
+    
+  }
+}
--- arrays/main.java
+++ arrays/main.java
@@ -2,19 +2,21 @@ import java.util.Scanner;
 
 public class main{
   public static void main(String[] args) {
-  int i;
-  int u;
+  int i=0;
   int somme=0;
   double moyenne=0;
   int nombreElem=0;
   int[][] tableauEntier={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+  final int LENGTH=tableauEntier.length;
+  //somme =78 et moyenne=6
 
-    for (i=0;i<tableauEntier.length ;i++ ) {
-      for (u=0;u<tableauEntier[i].length ;u++ ) {
-
+    for (int[]key :tableauEntier ) {
+      for (int subKey :key ) {
+        somme+=subKey;
+        nombreElem++;
       }
     }
     moyenne=somme/nombreElem;
     System.out.println("la somme vaut "+somme+" et la moyenne vaut"+moyenne);
-  }
+   }
 }
