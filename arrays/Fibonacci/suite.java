5b867a12ef8edb9bb03a6d65aa03fb8276a2267e
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-12 12:18:25 +0100

    done

--- arrays/Fibonacci/suite.java
+++ arrays/Fibonacci/suite.java
@@ -3,6 +3,17 @@
 public class suite{
   public static void main(String[] args) {
     int suite[]=new int[25];
-    
+    final int LIMIT=suite.length;
+    int somme=0;
+    suite[0]=0;
+    suite[1]=1;
+
+    for (int i=2;i<LIMIT ;i++ ) {
+      somme=suite[0]+suite[1];
+      suite[i]=i+suite[i-1];
+    }
+    for (int key :suite ) {
+      System.out.println(key);
+    }
   }
 }
