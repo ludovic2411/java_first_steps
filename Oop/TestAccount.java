4ac032d2d62a33b4668bb714e004002288ce9f2f
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-14 13:59:35 +0100

    create_TestAccount

--- Oop/TestAccount.java
+++ Oop/TestAccount.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
 
 class Account{
   int id;
   String name;
   Double amount;
-  
-}
+
+  void insert(int i,String n,Double amt){
+    id=i;
+    name=n;
+    amount=amt;
+  }
+
+  void deposit(Double amt){
+    amount+=amt;
+  }
+
+  void withdraw(Double amt){
+    if (amount<amt) {
+      System.out.println("You can't withdraw more money");
+    }else{
+      amount-=amt;
+    }
+  }
+    void checkAmount(Double amt){
+      System.out.println("You have an amount of"+amt+" on your account");
+    }
+    void display(){
+      System.out.println("The account"+id+" made by"+name+" is provided with "+amount+" euros");
+    }
+
+  }
