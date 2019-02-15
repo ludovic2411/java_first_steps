74e765e05ec0db964a32bcc0f8d8be15dc527491
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-15 14:33:14 +0100

    try/catch

---
+++ algo_final_test/Main.java
@@ -0,0 +1,13 @@
+
+public class Main{
+  public static void main(String[] args){
+    int x = 0, y = 5;
+  try {
+    final int DIV=y/x;
+    System.out.println(DIV);
+    System.out.println("Pourrais-je m'afficher ?!");
+  } catch(Exception e) {
+    System.out.println("Il y a eu une erreur");
+  }
+ }
+}
