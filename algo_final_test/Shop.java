3d3a0181a653d7a869dfdb84cb4122e709cb9305
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-18 14:07:52 +0100

    declare arrays

---
+++ algo_final_test/Shop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Shop{
+  public static void main(String[] args) {
+    int nbClients=0;
+    Double prixTotal=0.00;
+    String action;
+    Double productList[][]=new Double[20][20];
+    String productName[]={"","banane","kiwi","chicons","aubergines","tomates",
+    "pâtes","fraises","café","coca","thé",
+    "vin","cigarettes","chocolat","lasagnes","ganta",
+    "poisson","poivrons","sel","poivre", "saucisse"};
+
+    System.out.println(productName.length);
+  }
+}
