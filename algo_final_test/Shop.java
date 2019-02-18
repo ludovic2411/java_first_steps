06877cf931fa5721fa469ab9e68a48eaa9ad7133
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-18 15:10:02 +0100

    methods

--- algo_final_test/Shop.java
+++ algo_final_test/Shop.java
@@ -1,11 +1,29 @@
 import java.util.Scanner;
 
 public class Shop{
+
+  Boolean inputValidator(int productId){
+    if (productId>=1&&productId<22) {
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  void display(String productName[],Double productList[][],double recetteTotale){
+    for (int i=1;i<22 ;i++ ) {
+      if (productList[i][0]>0) {
+        System.out.println("Vous avez acheté "+productList[i][0]+" "+productName[i]+" vous devez payer "+recetteTotale);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     int nbClients=0;
     Double prixTotal=0.00;
-    String action;
+    String action="";
     Double productList[][]=new Double[21][21];
+    Scanner sc=new Scanner(System.in);
     //premier indice:prix second indice: nombre de fois acheté
     String productName[]={"","banane","kiwi","chicons","aubergines","tomates",
     "pâtes","fraises","café","coca","thé",
@@ -36,6 +54,14 @@ public class Shop{
 
     for (int i=0;i<22 ;i++ ) {
       productList[i][1]=0.00;
+    }
+
+    while (!action.equals("stop")) {
+      System.out.println("votre action?");
+      action=sc.nextLine();
+      while(!action.equals("reset")){
+
+      }
     }
   }
 }
