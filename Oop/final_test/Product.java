8b3e7155e37d8b402dc390d174b300c74c95ca81
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-02-19 15:19:13 +0100

    class_product_created

---
+++ Oop/final_test/Product.java
@@ -0,0 +1,14 @@
+
+class Product{
+  int id;
+  String name;
+  Double price;
+  int nbAchat;
+
+  Product(int i,String n, Double p, int nb){
+    id=i;
+    name=n;
+    price=p;
+    nbAchat=nb;
+  }
+}
