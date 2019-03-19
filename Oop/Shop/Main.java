fd70c18b856421716da901d0a19382200c6ea063
Ludovic Ceccotti <ludovic.ceccotti@gmail.com>
2019-03-19 15:29:38 +0100

    list-ok

--- Oop/Shop/Main.java
+++ Oop/Shop/Main.java
@@ -5,6 +5,12 @@ import produit.Produit;
 public class Main{
     public static void main(String[] args) {
     Magasin shop=new Magasin();
-    
+    shop.insertProductNons(11,"Poudre à lessiver",16.50,16,5,false);
+    shop.insertProductNons(12,"Oridanteur",1250.99,8,3,true);
+    shop.insertProductCons(13, "coca", 2.50, 5, false);
+    shop.insertProductCons(14,"Bananes",0.90,10,true);
+    shop.insertClient("Amaury","foo@bar.com");
+    shop.displayArticles();
+    shop.displayClients();
     }
 }
--- Oop/Shop/magasin/Magasin.java
+++ Oop/Shop/magasin/Magasin.java
@@ -18,15 +18,30 @@ public class Magasin{
       this.articles=new HashSet();
     }
 
+    public void insertProductCons(int reference, String libelle, double prixUnitaire, int stock,Boolean discountable){
+      articles.add(new Consommable(reference, libelle, prixUnitaire, stock, discountable));
+    }
+
+    public void insertProductNons(int reference, String libelle, double prixUnitaire, int stock,int dureeVie,Boolean discountable){
+      articles.add(new NonConsommable(reference,libelle,prixUnitaire,stock,dureeVie,discountable));
+    }
+
+    public void insertClient(String nom, String mail){
+      Client newClient=new Client(nom,mail);
+      clientList.put(newClient.getMailClient(),newClient);
+    }
+
     public void displayClients(){
       for (Map.Entry key:this.clientList.entrySet() ) {
         System.out.println(key.getKey()+"  "+key.getValue());
       }
     }
     public void displayArticles(){
-      Iterator <Produit> i=articles.iterator();
-      while (i.hasNext()) {
-        System.out.println(i.next());
+    for (Produit key :articles ) {
+      System.out.println(key.getLibelle()+" prix: "+key.getPrix()+" stock:  "+key.getStock());
       }
+    }
+    public void buy(){
+    System.out.println("buy");
     }
 }
